8a2b2fa05a8325a252d3370a551ec8d02d8756c0
Anva <nyura777@yandex.ru>
2022-05-24 19:47:17 +0600

    gallery finish

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,31 @@
+// массив с карточками
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
 // задаю переменные
 const openButton = document.querySelector('.profile__button-edit_open-popup');
 const popup = document.querySelector('.popup');
@@ -7,8 +35,31 @@ let profileName = document.querySelector('.profile__name');
 let profileBio = document.querySelector('.profile__bio');
 let nameInput = document.querySelector('.popup__input_type_name');
 let bioInput = document.querySelector('.popup__input_type_bio');
+const gallery = document.querySelector('.gallery');
+const listElement = document.querySelector('.gallery__list');
+const galleryTemplate = document.querySelector('#gallery-template').content;
 
-// функция открытия попапа с прописанными значениями из профиля
+function renderList(data) {
+  data.forEach((item) => renderItem(item));
+}
+
+function renderItem(data) {
+  const itemElement = galleryTemplate.cloneNode(true);/*клонируем содержимое template*/
+  const galleryImage = itemElement.querySelector(".gallery__image");
+  const galleryTitle = itemElement.querySelector(".gallery__title");
+  galleryImage.src = data.link; /*передаем значение атрибутов из массива*/
+  galleryTitle.textContent = data.name;
+  galleryImage.alt = data.name;
+  btnlikeElement = itemElement.querySelector('.gallery__like').addEventListener("click", function(event) { /*ставим лайк*/
+    event.target.classList.toggle('gallery__like_active');
+  })
+  listElement.append(itemElement); /*вставляемкарточки в конец списка*/
+}
+renderList(initialCards);/*загружаем 6 карточек из массива на сайт*/
+
+
+
+/* функция открытия попапа с прописанными значениями из профиля*/
 function togglePopup () {
   popup.classList.toggle('popup_opened')
   if (popup.classList.contains('popup_opened')) {
