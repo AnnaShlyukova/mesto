5b58a1938e50170f7abd37f61592fada9fd54871
Anva <nyura777@yandex.ru>
2022-05-26 20:12:13 +0600

    finish popupcontent

--- scripts/index.js
+++ scripts/index.js
@@ -28,47 +28,83 @@ const initialCards = [
 
 // задаю переменные
 const openButton = document.querySelector('.profile__button-edit_open-popup');
-const popup = document.querySelector('.popup');
+/*const popup = document.querySelector('.popup');*/
+const popupProfile = document.querySelector('.popup__profile');
+const popupGallery = document.querySelector('.popup__gallery');
+const popupImage = document.querySelector('.popup__image-view');
 const closeButton = document.querySelector('.popup__button-close');
 let formElement = document.querySelector('.popup__form');
 let profileName = document.querySelector('.profile__name');
 let profileBio = document.querySelector('.profile__bio');
-let nameInput = document.querySelector('.popup__input_type_name');
-let bioInput = document.querySelector('.popup__input_type_bio');
+let nameInput = document.querySelector('#profile-name');
+let bioInput = document.querySelector('#profile-bio');
+const openButtonadd = document.querySelector('.profile__button-add_open-popup');
+let nameImage = document.querySelector('#gallery-name');
+let linkImage = document.querySelector('#gallery-link');
 const gallery = document.querySelector('.gallery');
 const listElement = document.querySelector('.gallery__list');
 const galleryTemplate = document.querySelector('#gallery-template').content;
+const itemElement = galleryTemplate.cloneNode(true);
+
+
 
 function renderList(data) {
   data.forEach((item) => renderItem(item));
 }
-
-function renderItem(data) {
-  const itemElement = galleryTemplate.cloneNode(true);/*клонируем содержимое template*/
-  const galleryImage = itemElement.querySelector(".gallery__image");
-  const galleryTitle = itemElement.querySelector(".gallery__title");
-  galleryImage.src = data.link; /*передаем значение атрибутов из массива*/
-  galleryTitle.textContent = data.name;
-  galleryImage.alt = data.name;
-  btnlikeElement = itemElement.querySelector('.gallery__like').addEventListener("click", function(event) { /*ставим лайк*/
-    event.target.classList.toggle('gallery__like_active');
-  })
-  listElement.append(itemElement); /*вставляемкарточки в конец списка*/
+function deleteButtonItem (event) {
+  let buttonElement = event.target;
+  let listItemElement = buttonElement.closest(".gallery__item");
+  listItemElement.remove();
 }
-renderList(initialCards);/*загружаем 6 карточек из массива на сайт*/
 
-
-
+function likeButtonItem (event) {
+  let buttonElement = event.target;
+  buttonElement.classList.toggle('gallery__like_active');
+}
 /* функция открытия попапа с прописанными значениями из профиля*/
-function togglePopup () {
-  popup.classList.toggle('popup_opened')
-  if (popup.classList.contains('popup_opened')) {
+function togglePopup (event) {
+  popupProfile.classList.toggle('popup_opened');
+  if (popupProfile.classList.contains('popup_opened')) {
   nameInput.value = profileName.textContent;
   bioInput.value = profileBio.textContent;
 }
 }
 
-// функция обновления информации после внесения изменений в форму
+function openPopupGallery(event) {
+  popupGallery.classList.add('popup_opened');
+}
+function openPopupImage(event) {
+  popupImage.classList.add('popup_opened');
+}
+
+
+
+function renderItem(data) {
+  const itemElement = galleryTemplate.cloneNode(true);/*клонируем содержимое template*/
+  let galleryImage = itemElement.querySelector(".gallery__image");
+  const galleryTitle = itemElement.querySelector(".gallery__title");
+  galleryImage.src = data.link; /*передаем значение атрибутов из массива*/
+  galleryTitle.textContent = data.name;
+  galleryImage.alt = data.name;
+  let buttonlikeElement = itemElement.querySelector(".gallery__like");
+  buttonlikeElement.addEventListener('click', likeButtonItem); /*ставим лайк*/
+  let buttondeletElement = itemElement.querySelector(".gallery__button-delete");
+  buttondeletElement.addEventListener('click', deleteButtonItem);
+
+/*открываем попап с картинкой*/
+  itemElement.querySelector(".gallery__image").addEventListener('click', function (event){
+    openPopupImage();
+    popupImage.querySelector('.popup__image').src = `${event.target.src}`;
+    popupImage.querySelector('.popup__image').alt = `${event.target.alt}`;
+    popupImage.querySelector('.popup__image-caption').textContent = `${event.target.alt}`;
+  });
+
+  listElement.append(itemElement); /*вставляемкарточки в конец списка*/
+}
+renderList(initialCards);/*загружаем 6 карточек из массива на сайт*/
+
+
+// функция обновления информации после внесения изменений в форму*/
 function formSubmitHandler (evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
@@ -76,7 +112,10 @@ function formSubmitHandler (evt) {
   togglePopup();
 }
 
+
+
 // слушатели событий открытия попапа, закрытия попапа, сохранения изменений в попапе
-openButton.addEventListener('click', togglePopup)
+openButton.addEventListener('click', togglePopup);
 closeButton.addEventListener('click', togglePopup);
+openButtonadd.addEventListener('click', openPopupGallery);
 formElement.addEventListener('submit', formSubmitHandler);
